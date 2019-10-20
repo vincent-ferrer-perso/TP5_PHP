ffd1078bac37cb841cada551f1cc521ca87fb533
vincent-ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-10-20 11:07:12 +0200

    class cache et index.php

--- Cache.php
+++ Cache.php
@@ -1,7 +1,65 @@
 <?php
 
-
 class Cache
 {
+    private $page;
+    private $expiration;
+    private $buffer;
+
+    public function __construct($page){
+        $this->page = $page;
+    }
+
+    public function getBuffer()
+    {
+        return $this->buffer;
+    }
+    public function setBuffer($buffer)
+    {
+        $this->buffer = $buffer;
+    }
+
+    public function getExpiration()
+    {
+        return filemtime('cache/' . $this->getPage() . '.html');
+    }
+    public function setExpiration($expiration)  //$expiration lie filemtime($file) qui renvoie date derniere modif $file
+    {
+        $this->expiration = $expiration;
+    }
+
+    public function getPage()
+    {
+        return $this->page;
+    }
+    public function setPage($page)
+    {
+        $this->page = $page;
+    }
+
+    public function cacheView(){
+        if (file_exists(('cache/') . $this->getPage() . '.html')){
+            if ($this->getExpiration() < time() + 3600){
+                $this->setBuffer(readfile('cache/' . $this->getPage() . '.html'));
+            }else{
+                echo 'fichier cache trop ancien';
+            }
+        }else{
+            echo 'fichier cache non trouvé';
+        }
+    }
+
+    public function startBuffer(){
+        ob_start();
+    }
+
+
+    public function endBuffer(){
+        $this->setBuffer(ob_get_contents());
+        ob_end_clean();
+        $this->setPage(file_get_contents('cache/' . $this->getPage() .'.html', $this->getBuffer()));
+        echo $this->getBuffer();
+    }
+
 
 }
--- index.php
+++ index.php
@@ -1,1 +1,32 @@
 <?php
+
+    require  'Cache.php';
+
+    /*
+    * On instancie un nouvel objet cache et on lui passe en paramètre
+    * le nom de la page. Ce nom doit être unique pour chaque page car il va
+    * générer un fichier du même nom.
+    */
+    $cache = new Cache('index');
+
+    /*
+    * Si le cache est à jour, la méthode cacheView() l'affiche et le reste du code est ignoré.
+    */
+    $cache->cacheView();
+
+    /*
+    * La méthode startBuffer(), enregistre tout ce qui suit en mémoire tampon.
+    */
+    $cache->startBuffer();
+
+    /*
+    * Ici on affiche le code de la page, les requêtes SQL,etc.
+    */
+    echo 'Le corps de page';
+
+    /*
+    * La page est finie, on enregistre tout le contenu qu'elle génère.
+    */
+    $cache->endBuffer();
+
+    echo 'version sans cache';
